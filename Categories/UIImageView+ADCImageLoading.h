7922f185412f532a0ed3cfb872f1cc5ff8bed3da
Aaron Daub <aaron.d.daub+git@gmail.com>
2013-12-15 12:01:42 -0800

    Add documentation to ADCImageLoading

--- Categories/UIImageView+ADCImageLoading.h
+++ Categories/UIImageView+ADCImageLoading.h
@@ -15,6 +15,24 @@ typedef UIImage*(^IL_ImageProcessingBlock)(UIImage* image);
 
 @interface UIImageView (ADCImageLoading)
 
+
+/**
+ * -IL_setImageFromURL:placeInCache:dataToImage:processing:validity:completion pulls data from a URL, transforms that data
+ * into an image, processes that image, validates the processed image and then calls back on the main queue alerting you to
+ * whether or not this process succeeded. If the process is successful, this method will call -setImage: on the main queue with
+ * product of this pipeline.
+ *
+ * @param URL - this is the URL from which the image will be pulled. If the URL is a fileURL we'll check the in-memory cache and the caches directory (the caches URL ++ the last path component of this parameter). We won't go out to the network unless the URL is not a fileURL.
+ 
+ * @param shouldCache - if this is YES we will cache the unprocessed image in an in-memory NSCache as well as in the on-disk caches directory. If this is NO we won't cache the image anywhere.
+ 
+ * @param dataToImage - this is a block that is passed an NSData* and returns a UIImage*, if no block is supplied we get a UIImage* from calling -[UIImage imageWithData:].
+ 
+ * @param validity - this is a block that is passed the processed image and returns a BOOL indicating whether this image is acceptable and still relevant. If no block is specified we will assume any non-nil UIImage* is valid. If shouldCache is YES we will cache the image even if this block will return NO.
+ 
+ * @param completion - this is a block that will be called on the main queue with four parameters that indicate whether this operation was successful, and if applicable, where the resulting image came from as well as that image itself.
+ *
+ */
 - (void)IL_setImageFromURL:(NSURL*)URL placeInCache:(BOOL)shouldCache dataToImage:(IL_TransformDataIntoImageBlock)dataToImageBlock processing:(IL_ImageProcessingBlock)processingHandler validity:(IL_ImageValidityPredicateBlock)validityHandler completion:(IL_ImageLoadingCompletionBlock)completionHandler;
 
 @end
